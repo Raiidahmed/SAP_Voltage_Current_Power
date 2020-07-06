92c906c87d9eabd626e401a19d8d1146c842dd23
raiidahmed <Matanui123>
2020-07-05 21:04:14 -0400

    Fixed Voltage Reader

--- SoftAP_Current_Voltage_Power/SoftAP_Current_Voltage_Power.ino
+++ SoftAP_Current_Voltage_Power/SoftAP_Current_Voltage_Power.ino
@@ -1,9 +1,62 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
+/* Put your SSID & Password */
+const char* ssid = "NodeMCU";  // Enter SSID here
+const char* password = "12345678";  //Enter Password here
+
+/* Put IP Address details */
+IPAddress local_ip(192,168,1,1);
+IPAddress gateway(192,168,1,1);
+IPAddress subnet(255,255,255,0);
+
+ESP8266WebServer server(80);
+int SensorValue = 0;
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.softAP(ssid, password);
+  WiFi.softAPConfig(local_ip, gateway, subnet);
+  delay(100);
+  
+  server.on("/", handle_OnConnect);
+  server.begin();
+  
+  Serial.println("HTTP server started");
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  server.handleClient();
+}
 
+void handle_OnConnect() 
+{
+  server.send(200, "text/html", SendHTML(String(SensorValue))); 
+}
+
+int Get_Val(){
+  int Val = analogRead(A0);
+  return Val;
+}
+
+String SendHTML(String Value){
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr +="<head>";
+  ptr +="<META HTTP-EQUIV='refresh' CONTENT='1'>";
+  ptr +="<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr +="<title>Counter</title>\n";
+  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
+  ptr +="p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
+  ptr +="</style>\n";
+  ptr +="</head>\n";
+  ptr +="<body>\n";
+  ptr +="<h1>Voltage Display</h1>\n";
+  ptr +="<h3>Using Access Point(AP) Mode</h3>\n";
+  ptr +="<p>" + String(Get_Val()) + "V</p>";
+  ptr +="</body>\n";
+  ptr +="</html>\n";
+  return ptr;
 }
